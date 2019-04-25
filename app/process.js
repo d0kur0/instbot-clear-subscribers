3eeece0aa8bead8ef9cf259b06abc242cbf048c5
d0kur0 <dev.dokuro@gmail.com>
2019-04-25 15:42:44 +0300

    Создал базовый процесс который всё вызывает и запускает работу 'петли'

--- app/process.js
+++ app/process.js
@@ -1,1 +1,17 @@
-const auth = require('modules/auth');
+global._app = `${__dirname}`;
+
+const puppeteer       = require('puppeteer');
+const browserSettings = require(`${_app}/configs/browserSettings`);
+const auth            = require(`${_app}/modules/auth`);
+const loop            = require(`${_app}/modules/loop.js`);
+
+(async () => {
+  const browser = await puppeteer.launch(browserSettings);
+  const page    = await browser.newPage();
+
+  if (!await auth(page)) {
+    return await browser.close();
+  }
+
+  await loop(page);
+})();
